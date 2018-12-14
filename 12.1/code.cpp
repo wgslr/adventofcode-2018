527eca802b6da2172cab7cc731b8ef05a4cc9b38
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-12-14 08:17:51 +0100

    12.1: Generate switch for classifier

--- 12.1/code.cpp
+++ 12.1/code.cpp
@@ -1,11 +1,13 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 
 char parse(char pattern[])
 {
   unsigned char result = 0;
   for (int i = 0; pattern[i] != '\0'; ++i)
   {
+    // result |= (pattern[i] == '#' ? 0x01 : 0x00) << (8 - i - 1);
     result <<= 1;
     result |= pattern[i] == '#' ? 0x01 : 0x00;
   }
@@ -30,8 +32,27 @@ void print_pots(const unsigned char octet)
   print_bit(octet, '#', '.');
 }
 
-int main()
+void generate_classifier() {
+  char pattern[8];
+  char result;
+
+  printf("switch(%s)\n{\n");
+
+  while(scanf("%s => %c", pattern, &result) != EOF) {
+    unsigned char binary = parse(pattern);
+
+    printf("case 0x%X:\n\treturn %u;\n", binary, result == '#');
+  }
+  printf("}");
+}
+
+int main(int argc, char* argv[]) 
 {
+  if(argc > 1 && strcmp(argv[1], "generate") ==0 ) {
+    generate_classifier();
+    return 0;
+  }
+
   char pattern[] = ".#.##";
   print_hex(parse(pattern));
   printf("\n");
