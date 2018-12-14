c86239b7a1d7f33797bba0e495843418c8acf49f
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-12-14 13:45:08 +0100

    Clean up day 14 code

--- 14.2/code.c
+++ 14.2/code.c
@@ -3,13 +3,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-// const int INPUT = 110201;
-// const int INPUT = 2118;
 const int INPUT_DIGITS[] = {1, 1, 0, 2, 0, 1};
 const int INPUT_LEN = 6;
-// const int INPUT_DIGITS[] = {5, 1, 5, 8, 9};
-// const int INPUT_LEN = 5;
-// const int INPUT = 9;
 
 int length = 0;
 
@@ -19,17 +14,6 @@ typedef struct list
   struct list *next;
   struct list *prev;
 } list;
-
-// void push(list **l, int val)
-// {
-//   list *n = calloc(1, sizeof(list));
-//   n->next = *l;
-//   n->prev = (*l)->prev;
-
-//   (*l)->prev = n;
-//   n->val = val;
-//   *l = n;
-// }
 
 // returns pointer to the new element
 list *insert_after(list *l, int val)
@@ -41,7 +25,7 @@ list *insert_after(list *l, int val)
 
   n->next->prev = n;
   n->prev = l;
-  // fprintf(stderr, "add %d after %d\n", n->val, l->val);
+
   return n;
 }
 
@@ -67,39 +51,6 @@ void *add_score(list **rightmost, int val)
   ++length;
 }
 
-int list_length(list *l, int acc)
-{
-  int len = 1;
-  for (list *it = l->next; it != l; it = it->next)
-  {
-    ++len;
-  }
-  return len;
-}
-
-void print_all(int generation, list *leftmost, list *elf1, list *elf2)
-{
-  list *it = leftmost;
-  printf("%4d (%4d, %4d): ", generation, list_length(leftmost, 0), length);
-  do
-  {
-    if (it == elf1)
-    {
-      printf("(%d)", it->val);
-    }
-    else if (it == elf2)
-    {
-      printf("[%d]", it->val);
-    }
-    else
-    {
-      printf(" %d ", it->val);
-    }
-    it = it->next;
-  } while (it != leftmost);
-  printf("\n");
-}
-
 bool stop(list *rightmost)
 {
   for (int i = 0; i < INPUT_LEN; ++i)
@@ -117,7 +68,6 @@ int main()
   leftmost->prev = leftmost;
   leftmost->next = leftmost;
   leftmost->val = 3;
-  // printf("init\n");
 
   list *elf1, *elf2;
 
@@ -130,10 +80,8 @@ int main()
   length = 2;
 
   int generation = 1;
-  // int max = 10000000;
-  while (!stop(rightmost) && !stop(rightmost->prev) ) //&& generation < max )
+  while (!stop(rightmost) && !stop(rightmost->prev))
   {
-    // print_all(generation, leftmost, elf1, elf2);
     ++generation;
 
     int to_add = elf1->val + elf2->val;
@@ -141,27 +89,15 @@ int main()
     elf1 = steps(elf1, 1 + elf1->val);
     elf2 = steps(elf2, 1 + elf2->val);
   }
-  // print_all(generation, leftmost, elf1, elf2);
+
   if (stop(rightmost))
   {
-    printf("%d\n", length - INPUT_LEN);
+    printf("last matched: %d\n", length - INPUT_LEN);
   }
   else if (stop(rightmost->prev))
   {
     printf("previous matched: %d\n", length - INPUT_LEN - 1);
-  } else {
-    printf("reached limit\n");
   }
-  printf("generations: %d\n", generation);
-  // print_all(generation, leftmost, elf1, elf2);
-
-  // list *result = steps(leftmost, INPUT);
-  // for (int i = 0; i < 10; ++i)
-  // {
-  //   printf("%d", result->val);
-  //   result = result->next;
-  // }
-  // printf("\n");
 
   return 0;
 }
