4a9868a035348a08415ae04f60e297e282052604
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-12-15 09:59:40 +0100

    12.1: Implement 5-bit getter

--- 12.1/code.cpp
+++ 12.1/code.cpp
@@ -3,8 +3,12 @@
 #include <cstring>
 
 const int SIZE = 200;
-const int ZERO = SIZE * 8 / 2; // ofset
 unsigned char *state[2];
+
+// counting bits
+const int ZERO = SIZE * 8 / 2; // ofset
+int first = ZERO;
+int last = ZERO;
 
 char parse(char pattern[])
 {
@@ -93,10 +97,9 @@ inline unsigned char next(unsigned char current)
   }
 }
 
-void set(unsigned char*  st , bool val, int pos)
+inline void set(unsigned char *st, bool val, int pos)
 {
-  printf("Set %d to %d\n", pos, val);
-  const int octet = pos >> 3;
+  const int octet = pos / 8;
   const int bit = pos % 8;
   if (val)
   {
@@ -115,7 +118,28 @@ void read_input()
 
   for (int i = 0; buff[i] != '\0'; ++i)
   {
-    set(state[0], buff[i] == '#', ZERO + i );
+    set(state[0], buff[i] == '#', ZERO + i);
+    if('#' == buff[i])
+      last = ZERO + i;
+  }
+}
+
+// gets the sourroundings of given pos
+unsigned char get5(unsigned char *st, int pos)
+{
+  const int first = pos - 2;
+  const int octet = first / 8;
+  const int bit = first % 8;
+  if (bit <= 3)
+  { // contained in one octet
+    // clean irrelevant bits to the left and then move to the rightmost bits
+    return (st[octet] & (0xFF >> bit)) >> (3 - bit);
+  }
+  else
+  {
+    const unsigned overflow = bit - 3;
+    const unsigned char result = (st[octet] & (0xFF >> bit)) << overflow;
+    return result | st[octet + 1] >> (8 - overflow);
   }
 }
 
@@ -127,14 +151,35 @@ int main(int argc, char *argv[])
     return 0;
   }
 
-  state[0] = (unsigned char*)calloc(SIZE, sizeof(unsigned char));
-  state[1] = (unsigned char*)calloc(SIZE, sizeof(unsigned char));
+  state[0] = (unsigned char *)calloc(SIZE, sizeof(unsigned char));
+  state[1] = (unsigned char *)calloc(SIZE, sizeof(unsigned char));
+
+  // unsigned char test[2] = {0x01, 0xC0};
+  // printf("%x %x\n", test[0], test[1]);
+  // printf("%u\n", *((short *)test));
+  // printf("%u\n", (*((short *)test)) << 1);
+  // printf("%u\n\n", sizeof(short));
+
+  // print_bit(0xF0, '1', '0');
+  // printf("\n");
+  // print_bit(0xF0 << -1, '1', '0');
+  // printf("\n");
+  // print_bit(0xFF >> 1, '1', '0');
+  // printf("\n");
+
+  // ("%x %x\n", 0xF00F, 0xF00F << -1);
 
   read_input();
   for (int i = 0; i < SIZE; ++i)
   {
     print_pots(state[0][i]);
     printf(" ");
+  }
+
+    printf("\n\n");
+  for(int i = first; i <= last; ++i) {
+    print_pots(get5(state[0], i));
+    printf("\n");
   }
 
   return 0;
