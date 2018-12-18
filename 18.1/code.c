f093ee52d59d7652ffd335b14858d58057692e13
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-12-18 08:41:58 +0100

    18.1: Load input

--- 18.1/code.c
+++ 18.1/code.c
@@ -14,9 +14,61 @@ typedef enum
   YARD
 } field;
 
+void load(field state[HEIGHT][WIDTH])
+{
+  char line[WIDTH];
+  for (int y = 0; y < HEIGHT; ++y)
+  {
+    scanf("%s\n", line);
+    for (int x = 0; x < WIDTH; ++x)
+    {
+      switch (line[x])
+      {
+      case '.':
+        state[y][x] = GROUND;
+        break;
+      case '|':
+        state[y][x] = TREE;
+        break;
+      case '#':
+        state[y][x] = YARD;
+        break;
+      default:
+        fprintf(stderr, "Unexpected symbol %c\n", line[x]);
+      }
+    }
+  }
+}
+
+void display(field state[HEIGHT][WIDTH])
+{
+  for (int y = 0; y < HEIGHT; ++y)
+  {
+    for (int x = 0; x < WIDTH; ++x)
+    {
+      switch (state[y][x])
+      {
+      case GROUND:
+        printf(".");
+        break;
+      case TREE:
+        printf("|");
+        break;
+      case YARD:
+        printf("#");
+        break;
+      }
+    }
+    printf("\n");
+  }
+}
+
 int main()
 {
+  field state[2][HEIGHT][WIDTH];
 
+  load(state[0]);
+  display(state[0]);
 
   return 0;
 }
