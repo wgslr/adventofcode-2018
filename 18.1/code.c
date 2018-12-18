f3af5453ad3b6c2891987a8e0154548c9eac662f
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-12-18 08:36:04 +0100

    Fetch day 18 input

---
+++ 18.1/code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+const int LIMIT = 10;
+const int WIDTH = 50;
+const int HEIGHT = 50;
+
+typedef enum
+{
+  GROUND,
+  TREE,
+  YARD
+} field;
+
+int main()
+{
+
+
+  return 0;
+}
